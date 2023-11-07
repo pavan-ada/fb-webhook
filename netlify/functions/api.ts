0a7935915efb2ab324ca934fcf6c69649fee6d20
pavan-ada <pavan.kumar@ada-asia.com>
2023-11-08 00:23:54 +0800

    created webhook route for post

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -4,8 +4,21 @@ import serverless from "serverless-http";
 const api = express();
 
 const router = Router();
+router.get("/", (req, res) => res.send("Reached webhook endpoint!"));
 router.get("/hello", (req, res) => res.send("Hello World!"));
-router.get("/webhook", (req, res) => res.send("Reached webhook endpoint!"));
+router.get('/webhook', function(req, res) {
+    if (
+        req.query['hub.mode'] == 'subscribe') {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.sendStatus(400);
+    }
+});
+
+router.post("/webhook", function(req, res) {
+    console.log('Incoming webhook: ' + JSON.stringify(req.body));
+    res.sendStatus(200);
+});
 
 api.use("/api/", router);
 
